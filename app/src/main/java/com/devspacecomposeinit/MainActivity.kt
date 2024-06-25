c6812edc8203ffdfc2f9397760f942d0a1f8f3f0
BrunoHPBarbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2024-06-25 11:27:12 +0100

    criacao do artist list

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -3,13 +3,36 @@ package com.devspacecomposeinit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devspacecomposeinit.ui.theme.ComposeInitTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +45,129 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    val pablo = Artist(
+                        name = "Pablo Picasso",
+                        lastSeenOnline = "2 min ago...",
+                        image = R.drawable.ic_pablo_picasso,
+                        art = R.drawable.ic_beijo
+                    )
+                    val vanGogh = Artist(
+                        name = "Vincent Van Gogh",
+                        lastSeenOnline = "2 min ago...",
+                        image = R.drawable.ic_vincent_van_gogh,
+                        art = R.drawable.ic_starry_night
+                    )
+                    val leonardo = Artist(
+                        name = "Leonardo da Vinci",
+                        lastSeenOnline = "2 min ago...",
+                        image = R.drawable.ic_leonardo_da_vinci,
+                        art = R.drawable.ic_mona_lisa
+                    )
+                    val salvadorDali = Artist(
+                        name = "Salvador Dali",
+                        lastSeenOnline = "2 min ago...",
+                        image = R.drawable.ic_salvador_dali,
+                        art = R.drawable.ic_persistence_of_memory
+                    )
+                    val artists = listOf(vanGogh, leonardo, salvadorDali, pablo)
+                    LazyColumn {
+                        items(artists) { artist ->
+                            ArtistCard(artist = artist,
+                                onClick = {
+                                    println("Bruno" + artist.name)
+                                }
+                            )
+
+                        }
+                    }
                 }
+
             }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeInitTheme {
-        Greeting("Android")
+fun ArtistCard(
+    artist: Artist,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick)
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.FillWidth,
+                painter = painterResource(id = artist.image),
+                contentDescription = "artist image"
+            )
+            Spacer(modifier = Modifier.size(14.dp))
+            Column {
+                Text(
+                    text = artist.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+
+                Text(
+                    text = artist.lastSeenOnline,
+                    color = Color.Gray
+                )
+            }
+
+
+        }
+        Card(
+            modifier = Modifier
+                .padding(8.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = artist.art), contentDescription = "art"
+            )
+
+        }
     }
 }
+
+data class Artist(
+    val name: String,
+    val lastSeenOnline: String,
+    @DrawableRes val image: Int,
+    @DrawableRes val art: Int,
+)
+
+@Preview(showSystemUi = true)
+@Composable
+fun ArtisCardPreview() {
+    ComposeInitTheme {
+        val artist = Artist(
+            name = "Alfred",
+            lastSeenOnline = "3 min ..",
+            art = R.drawable.ic_pablo_picasso,
+            image = R.drawable.ic_starry_night
+        )
+        ArtistCard(artist = artist,
+            onClick = {
+
+            })
+
+    }
+}
+
+
+
